a5da1e5079c6dce6338e4b128605fd4cc93d7aa5
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-09-11 21:56:09 +0300

    решена 6 задача!

--- zadacha6Weather.py
+++ zadacha6Weather.py
@@ -4,14 +4,16 @@
 import requests
 import json
 
-APIkey = "205f114eaf923c8a254f6d985fb522ae"
-cituName = 'Липецк'
+cituName = input('Введите город (пример Липецк): ')
 
-api_url = f'https://api.openweathermap.org/data/2.5/weather?q={cituName}&appid={APIkey}'
+api_url = f'https://api.weatherapi.com/v1/current.json?key=f9041ec3e97e4629a9c124428231109&q={cituName}&aqi=no'
 response = requests.get(api_url)
 
 if response.status_code == 200:
-    data = json.load(response.text)
-    print(data)
+    data = response.json()
+    print(f'Было обнавлено - {data["current"]["last_updated"]}')
+    print(f'{data["location"]["name"]}, {data["location"]["country"]}')
+    print(f'Погода: {data["current"]["temp_c"]} градусов')
+    print(f'Влажность {data["current"]["humidity"]}')
 else:
     print("Ошибка при выполнении запроса:", response.status_code)
