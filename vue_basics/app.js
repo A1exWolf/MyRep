2ff394c95a54ba88a22c004b41eab5e636c14592
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-09-28 15:44:51 +0300

    заметки v2

--- vue_basics/app.js
+++ vue_basics/app.js
@@ -2,7 +2,33 @@ const App = {
     data() {
         return {
             title: "Список заметок",
-            placeholderString: 'Введите название заметки'
+            placeholderString: 'Введите название заметки',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
+    methods: {
+        addNewNote() {
+            if (this.inputValue !== ""){
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        removeNote(idx) {
+            this.notes.splice(idx,1)
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value) {
+
         }
     }
 }
