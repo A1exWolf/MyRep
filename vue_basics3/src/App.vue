4b419766c9f757510a196df98f54f676fdc79e07
Alexander Volkov <81536837+A1exWolf@users.noreply.github.com>
2023-10-03 13:30:49 +0300

    close project(

---
+++ vue_basics3/src/App.vue
@@ -0,0 +1,50 @@
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+
+export default {
+  components: {
+    PostForm, PostList
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Javascript', body: 'Описание поста'},
+        {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+        {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
+      ],
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+  }
+}
+</script>
+
+<template>
+  <div class="app">
+    <post-form
+        @create="createPost"
+    />
+    <post-list
+        :posts="posts"
+    />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+
+
+</style>
